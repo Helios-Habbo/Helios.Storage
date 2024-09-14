ffd1acff14cd0a37227540bed2a1b41fb4159e5d
Quackster <quacksterr.15@gmail.com>
2024-09-14 11:24:11 +1000

    Fix subscription calculation

--- Helios.Storage/Models/Subscription/SubscriptionData.cs
+++ Helios.Storage/Models/Subscription/SubscriptionData.cs
@@ -13,7 +13,6 @@ namespace Helios.Storage.Models.Subscription
         public virtual long SubscriptionAge { get; set; }
         public virtual DateTime SubscriptionAgeLastUpdated { get; set; }
 
-        [NotMapped]
         public virtual int MonthsLeft
         {
             get
@@ -21,12 +20,20 @@ namespace Helios.Storage.Models.Subscription
                 if (DateTime.Now > ExpireDate)
                     return 0;
 
-                TimeSpan ts = ExpireDate.Subtract(DateTime.Now);
-                return ts.TotalDays > 0 ? (int)ts.TotalDays / 30 : 0;
+                DateTime now = DateTime.Now;
+                DateTime expireDate = ExpireDate;
+
+                int months = (expireDate.Year - now.Year) * 12 + expireDate.Month - now.Month;
+
+                if (expireDate.Day < now.Day)
+                {
+                    months--;
+                }
+
+                return months;
             }
         }
 
-        [NotMapped]
         public virtual int DaysLeft
         {
             get
@@ -34,17 +41,25 @@ namespace Helios.Storage.Models.Subscription
                 if (DateTime.Now > ExpireDate)
                     return 0;
 
-                TimeSpan ts = ExpireDate.Subtract(DateTime.Now);
-                var monthsLeft = MonthsLeft;
+                DateTime now = DateTime.Now;
+                DateTime expireDate = ExpireDate;
 
-                // Remove the months left from the remaining time, so we just get the days left..
-                if (monthsLeft > 0)
+                // Calculate total days between now and expireDate
+                int totalDays = (expireDate - now).Days;
+
+                // Calculate the number of months remaining
+                int months = (expireDate.Year - now.Year) * 12 + expireDate.Month - now.Month;
+
+                if (expireDate.Day < now.Day)
                 {
-                    var expireDate = ExpireDate.AddDays(monthsLeft * 30 * -1); // Turn the months left into days into a negative number by multiplying by -1
-                    ts = expireDate.Subtract(DateTime.Now);
+                    months--;
                 }
 
-                return ts.Days;
+                // Calculate the number of days left after accounting for full months
+                DateTime firstOfNextMonth = now.AddMonths(months + 1).Date;
+                int daysInNextMonth = (expireDate - firstOfNextMonth).Days;
+
+                return totalDays - (months * 30) + daysInNextMonth; // Adjust the days calculation
             }
         }
     }
